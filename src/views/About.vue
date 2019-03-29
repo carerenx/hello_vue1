7581e476f7a6353439652be8c827916e052891bf
蔡忍 <pp313102108@qq.com>
2019-03-29 15:49:19 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <P>{{msg}}</P>
+  </div>
+</template>
+<script>
+  import store from '@/store'
+  export default {
+    name:'about',
+    data(){
+      return{
+        msg:store.state.count
+      }
+    }
+  }
+</script>
---
+++ src/views/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="info">
+    <h1>This is an info page</h1>
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+<script>
+  import store from '@/store'
+  export default {
+    name: 'Info',
+    methods: {
+      add() {
+        console.log('add Even from info')
+        store.commit('increase')
+        alert('alter from info')
+      }
+    }
+  };
+</script>
